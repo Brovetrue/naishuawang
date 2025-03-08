39012a058d366332706ace82442c7928fbe94ead
Chiu <1391575453@qq.com>
2025-03-08 20:36:14 +0800

    修复bugs 1、修复使用自定义权限校验时不能读到SessionID 2、修复使用Sa-Token时未能读到有效数据 3、修复没有启动类 4、修复本地包未打包

--- naishuawang-next-backend/src/main/java/com/chiu/naishuawangnextbackend/satoken/SaTokenConfigure.java
+++ naishuawang-next-backend/src/main/java/com/chiu/naishuawangnextbackend/satoken/SaTokenConfigure.java
@@ -6,14 +6,16 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+/**
+ * Sa-Token 全局拦截器（为了支持权限注解）
+ */
 @Configuration
 public class SaTokenConfigure implements WebMvcConfigurer {
-    // 注册拦截器
+
+    // 注册 Sa-Token 拦截器，打开注解式鉴权功能
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
-        // 注册 Sa-Token 拦截器，校验规则为 StpUtil.checkLogin() 登录校验。
-        registry.addInterceptor(new SaInterceptor(handle -> StpUtil.checkLogin()))
-                .addPathPatterns("/**")
-                .excludePathPatterns("/user/doLogin"); 
+        // 注册 Sa-Token 拦截器，打开注解式鉴权功能
+        registry.addInterceptor(new SaInterceptor()).addPathPatterns("/**");
     }
 }
